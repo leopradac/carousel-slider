b2c9dfb932c51d3e77850593becabe8e21cfb131
leo.prada90 <leo.prada90@gmail.com>
2024-12-12 04:46:24 +0000

    add all

---
+++ data/icons.js
@@ -0,0 +1,29 @@
+const icons = [
+    {
+        name: 'aws',
+        width: '100px',
+        src: '/icons/aws.png'
+    },
+    {
+        name: 'vue',
+        width: '50px',
+        src: '/icons/vue.png'
+    },
+    {
+        name: 'ts',
+        width: '50px',
+        src: '/icons/ts.png'
+    },
+    {
+        name: 'nuxt',
+        width: '100px',
+        src: '/icons/nuxt.png'
+    },
+    {
+        name: 'git',
+        width: '100px',
+        src: '/icons/git.png'
+    },
+
+]
+export default icons
